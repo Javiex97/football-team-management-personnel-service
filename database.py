d40fa33f39264b99f033db6c550ace64a5d40770
Javiex97 <u20211197371@usco.edu.co>
2025-05-24 20:49:45 -0500

    ms

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import create_engine, MetaData
+from databases import Database
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = "postgresql://postgres:WbnS5utrc1Gayg35@db.olzrkkjzqltovkvxeggb.supabase.co:5432/postgres"
+
+database = Database(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+metadata = MetaData() 
